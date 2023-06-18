2eaa78d9367beb565100e61515fa34c2d49d5733
SamDayCode <samwood97@hotmail.com>
2023-06-18 17:01:55 +0800

    First commit of initial blank files.

---
+++ senor.py
@@ -0,0 +1,8 @@
+from sense_hat import SenseHat
+
+class Sensor:
+    def __init__(self):
+        self.sense_hat = SenseHat()
+
+    def get_temperature(self):
+        return self.sense_hat.get_temperature()
